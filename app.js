c933d87e89e7c38485df7b3222c4bff335e69fe0
bdj <11084184@qq.com>
2013-09-27 18:01:34 +0800

    修改运行配置，运行后直接打开html文件。去掉多余的Views和Route目录

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
+//var routes = require('./routes');
+//var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
@@ -13,7 +13,7 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
+//app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
@@ -21,7 +21,7 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
-app.use(app.router);
+//app.use(app.router);
 app.use(express.static(path.join(__dirname, 'app')));
 
 // development only
