a0fbfd863e7562276b902e3bf2cc1353559d522e
bdj <11084184@qq.com>
2013-10-05 06:29:10 +0800

    消除单元测试的两个警告

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -4,7 +4,6 @@ module.exports = function(config){
 
     files : [
       'app/lib/angular/angular.js',
-      'app/lib/angular/angular-*.js',
       'app/lib/angular-mocks/angular-mocks.js',
       'app/js/**/*.js',
       'test/unit/**/*.js'
@@ -17,16 +16,11 @@ module.exports = function(config){
     browsers : ['PhantomJS'],
 
     plugins : [
-            'karma-junit-reporter',
             'karma-phantomjs-launcher',
             'karma-chrome-launcher',
             'karma-firefox-launcher',
             'karma-jasmine'       
             ],
 
-    junitReporter : {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
 
 })}
